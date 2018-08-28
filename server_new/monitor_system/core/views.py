ca2bbdbe4141ef4c73734ef607f0b9680d60ce31
Xuelan <421805440@qq.com>
2018-08-28 20:46:48 +1000

    ajax

--- server_new/monitor_system/core/views.py
+++ server_new/monitor_system/core/views.py
@@ -3,6 +3,8 @@ from monitor_system.models import Instrument,Sample
 import time
 from collections import defaultdict
 import datetime
+import json
+import simplejson
 
 core = Blueprint('core',__name__)
 
@@ -13,89 +15,226 @@ def index():
 @core.route('/detail')
 def detail():
     all_instruments = [instrument.name for instrument in Instrument.query.all()]
-    if request.args.get('sele'):
-        selected_ins = request.args.get('sele')
-        print (selected_ins)
+    if request.method == 'GET' and request.args.get('sidebar', ''):
+        sidebar = request.args.get("sidebar", None)
+        sidebarList = ["multiSidebar","singleSidebar"]
+        if sidebar in sidebarList:
+            return render_template(sidebar + ".html",all_instruments=all_instruments)
 
-    return render_template("new_detail.html", all_instruments=all_instruments, start_time=0)
-
-
-@core.route('/detail/<selected_ins>', methods=['GET','POST'])
-def ins_detail(selected_ins):
-
-    ins_data = defaultdict(dict)
-    all_instruments = [instrument.name for instrument in Instrument.query.all()]
-
-   #if chose via date picker
-    if request.method=='POST' and request.form.get("start_time", None) and request.form.get("end_time", None):
-        start = request.form['start_time']
-        end = request.form['end_time']
-        start = datetime.datetime.strptime(start, "%Y-%m-%d").date()
-        end = datetime.datetime.strptime(end, "%Y-%m-%d").date()
-        # data of selected instrument
-        ins_samples = Sample.query.filter(Sample.instrument==selected_ins, Sample.actual_start<=end, Sample.actual_end>=start).all()
-        print ("time picked!")
-    # if chose a radio
-    elif request.method=="POST" and request.form.get("span", None):
-        print ("in the radio!!")
-        span = request.form.get("span")
-        print (span)
+    if request.method == 'GET' and request.args.get('multi', '')=="multi":
+        selected_instruments = json.loads(request.args.get('selected_instruments'))
+        # data of selected instruments in previous one week
         end = datetime.datetime.now()
-        if span == "1week":
-            start = end - datetime.timedelta(days=7)
-            # data of selected instrument in previous one week
-        elif span == "1month":
-            start = end - datetime.timedelta(days=30)
-        elif span == "6months":
-            start = end - datetime.timedelta(days=180)
-        else:
-            start = end - datetime.timedelta(days=365)
+        start = end - datetime.timedelta(days=20)
+        print (end)
 
-        ins_samples = Sample.query.filter(Sample.instrument == selected_ins, Sample.actual_start <= end,
+        info = defaultdict(dict)
+        for instrument in selected_instruments:
+            ins_samples = Sample.query.filter(Sample.instrument == instrument, Sample.actual_start <= end,
                                           Sample.actual_end >= start).all()
+            stList = []
+            etList = []
+            print (instrument)
+            # instrumentDic = defaultdict(dict)
+            # info[instrument] = instrumentDic
+            if ins_samples:
+                for sample in ins_samples:
+                    st = sample.actual_start
+                    st = int(time.mktime(st.timetuple()) * 1000)
+                    et = sample.actual_end
+                    et = int(time.mktime(et.timetuple()) * 1000)
+                    stList.append(st)
+                    etList.append(et)
+                info[instrument]["start"] = stList
+                info[instrument]["end"] = etList
+        end = int(time.mktime(end.timetuple()) * 1000)
+        start = int(time.mktime(start.timetuple()) * 1000)
+                # info[instrument] =instrumentDic
+        print (info)
+        # transfer to string
+        info = simplejson.dumps(info)
+        print (info)
+        # print (type(info))
+        return render_template('multiGraph.html',info=info, start_time=start, end_time=end)
 
-    else:
-        print ("default")
-        end = datetime.datetime.now()
-        start = end - datetime.timedelta(days=7)
-        # data of selected instrument in previous one week
-        ins_samples = Sample.query.filter(Sample.instrument == selected_ins, Sample.actual_start <= end,
-                                          Sample.actual_end >= start).all()
 
-    end = int(time.mktime(end.timetuple()) * 1000)
-    start = int(time.mktime(start.timetuple()) * 1000 )
 
-    data= []
+    elif request.method == 'GET' and request.args.get('name', ''):
+        print("in")
+        selected_instrument = request.args.get('name', '')
+        print(selected_instrument)
+        if selected_instrument in all_instruments:
+            ins_data = defaultdict(dict)
 
-    selected_span = end - start
-    running_time = 0
-    #collet the start and finish time of each running
-    for sample in ins_samples:
-        st = sample.actual_start
-        print (st)
+            # if choose via date picker
+            if request.args.get("start_time", None) and request.args.get("end_time", None):
+                start = request.args.get('start_time')
+                end = request.args.get('end_time')
+                start = datetime.datetime.strptime(start, "%Y-%m-%d").date()
+                end = datetime.datetime.strptime(end, "%Y-%m-%d").date()
+                # data of selected instrument
+                ins_samples = Sample.query.filter(Sample.instrument == selected_instrument, Sample.actual_start <= end,
+                                                  Sample.actual_end >= start).all()
+                print("time picked!")
 
-        # st = datetime.datetime.strptime(st, "%Y-%m-%d %H:%M:%S.%f")
-        st = int(time.mktime(st.timetuple()) * 1000 )
-        # st = int(time.mktime(st.timetuple()) * 1000 + st.microsecond / 1000)
-        et = sample.actual_end
-        print (et)
-        # et = datetime.datetime.strptime(et, "%Y-%m-%d %H:%M:%S.%f")
-        et = int(time.mktime(et.timetuple()) * 1000)
-        # et = int(time.mktime(et.timetuple()) * 1000 + et.microsecond / 1000)
-        if st <= start:
-            st = start
-        if et >= end:
-            et = end
-        data.append([st, 1])
-        data.append([et, 0])
 
-        running_time += et - st
+            # if chose an span option
+            elif request.args.get("span",None):
+                print("in the radio!!")
+                span = request.args.get("span")
+                print(span)
+                end = datetime.datetime.now()
+                if span == "1week":
+                    start = end - datetime.timedelta(days=7)
+                    # data of selected instrument in previous one week
+                elif span == "1month":
+                    start = end - datetime.timedelta(days=30)
+                elif span == "6months":
+                    start = end - datetime.timedelta(days=180)
+                else:
+                    start = end - datetime.timedelta(days=365)
 
-    ratio = round(running_time / selected_span * 100, 2)
-    rest_ratio = round(100 - ratio, 2)
-    ins_data['ratio'] = ratio
-    ins_data['rest_ratio'] = rest_ratio
-    ins_data['data'] = data
+                ins_samples = Sample.query.filter(Sample.instrument == selected_instrument, Sample.actual_start <= end,
+                                                  Sample.actual_end >= start).all()
 
-    count_sample = len(ins_samples)
-    return render_template('new_detail.html', start_time=start, end_time=end, ins_data=ins_data, all_instruments=all_instruments, selected_ins=selected_ins)
+            # if only pick an instrument
+            else:
+                print ("default!")
+                end = datetime.datetime.now()
+                start = end - datetime.timedelta(days=7)
+                # data of selected instrument in previous one week
+                ins_samples = Sample.query.filter(Sample.instrument == selected_instrument, Sample.actual_start <= end,
+                                                  Sample.actual_end >= start).all()
+
+
+            end = int(time.mktime(end.timetuple()) * 1000)
+            start = int(time.mktime(start.timetuple()) * 1000)
+
+            data = []
+
+            selected_span = end - start
+            running_time = 0
+            # collet the start and finish time of each running
+            for sample in ins_samples:
+                st = sample.actual_start
+                print(st)
+
+                # st = datetime.datetime.strptime(st, "%Y-%m-%d %H:%M:%S.%f")
+                st = int(time.mktime(st.timetuple()) * 1000)
+                # st = int(time.mktime(st.timetuple()) * 1000 + st.microsecond / 1000)
+                et = sample.actual_end
+                print(et)
+                # et = datetime.datetime.strptime(et, "%Y-%m-%d %H:%M:%S.%f")
+                et = int(time.mktime(et.timetuple()) * 1000)
+                # et = int(time.mktime(et.timetuple()) * 1000 + et.microsecond / 1000)
+                if st <= start:
+                    st = start
+                if et >= end:
+                    et = end
+                data.append([st, 1])
+                data.append([et, 0])
+
+                running_time += et - st
+
+            # ratio = round(running_time / selected_span * 100, 2)
+            # print (running_time)
+            # print (selected_span)
+            # rest_ratio = round(100 - ratio, 2)
+            uptime_hours = round(running_time/3600000,2)
+            downtime_hours = round((selected_span-running_time)/3600000,2)
+            ins_data['uptime'] = uptime_hours
+            ins_data['downtime'] = downtime_hours
+            # ins_data['ratio'] = ratio
+            # ins_data['rest_ratio'] = rest_ratio
+            ins_data['data'] = data
+
+            return render_template('graph.html', start_time=start, end_time=end, ins_data=ins_data,selected_instrument=selected_instrument)
+
+    return render_template("new_detail.html", all_instruments=all_instruments)
+
+
+# @core.route('/sidebar')
+# def multiSidebar():
+#     all_instruments = [instrument.name for instrument in Instrument.query.all()]
+
+    # else:
+        # pass
+# @core.route('/detail/<selected_ins>', methods=['GET','POST'])
+# def ins_detail(selected_ins):
+#
+#     ins_data = defaultdict(dict)
+#     all_instruments = [instrument.name for instrument in Instrument.query.all()]
+#
+#    #if chose via date picker
+#     if request.method=='POST' and request.form.get("start_time", None) and request.form.get("end_time", None):
+#         start = request.form['start_time']
+#         end = request.form['end_time']
+#         start = datetime.datetime.strptime(start, "%Y-%m-%d").date()
+#         end = datetime.datetime.strptime(end, "%Y-%m-%d").date()
+#         # data of selected instrument
+#         ins_samples = Sample.query.filter(Sample.instrument==selected_ins, Sample.actual_start<=end, Sample.actual_end>=start).all()
+#         print ("time picked!")
+#     # if chose a radio
+#     elif request.method=="POST" and request.form.get("span", None):
+#         print ("in the radio!!")
+#         span = request.form.get("span")
+#         print (span)
+#         end = datetime.datetime.now()
+#         if span == "1week":
+#             start = end - datetime.timedelta(days=7)
+#             # data of selected instrument in previous one week
+#         elif span == "1month":
+#             start = end - datetime.timedelta(days=30)
+#         elif span == "6months":
+#             start = end - datetime.timedelta(days=180)
+#         else:
+#             start = end - datetime.timedelta(days=365)
+#
+#         ins_samples = Sample.query.filter(Sample.instrument == selected_ins, Sample.actual_start <= end,
+#                                           Sample.actual_end >= start).all()
+#
+#     else:
+#         print ("default")
+#         end = datetime.datetime.now()
+#         start = end - datetime.timedelta(days=7)
+#         # data of selected instrument in previous one week
+#         ins_samples = Sample.query.filter(Sample.instrument == selected_ins, Sample.actual_start <= end,
+#                                           Sample.actual_end >= start).all()
+#
+#     end = int(time.mktime(end.timetuple()) * 1000)
+#     start = int(time.mktime(start.timetuple()) * 1000 )
+#
+#     data= []
+#
+#     selected_span = end - start
+#     running_time = 0
+#     #collet the start and finish time of each running
+#     for sample in ins_samples:
+#         st = sample.actual_start
+#         print (st)
+#
+#         # st = datetime.datetime.strptime(st, "%Y-%m-%d %H:%M:%S.%f")
+#         st = int(time.mktime(st.timetuple()) * 1000 )
+#         # st = int(time.mktime(st.timetuple()) * 1000 + st.microsecond / 1000)
+#         et = sample.actual_end
+#         print (et)
+#         # et = datetime.datetime.strptime(et, "%Y-%m-%d %H:%M:%S.%f")
+#         et = int(time.mktime(et.timetuple()) * 1000)
+#         # et = int(time.mktime(et.timetuple()) * 1000 + et.microsecond / 1000)
+#         if st <= start:
+#             st = start
+#         if et >= end:
+#             et = end
+#         data.append([st, 1])
+#         data.append([et, 0])
+#
+#         running_time += et - st
+#
+#     ratio = round(running_time / selected_span * 100, 2)
+#     rest_ratio = round(100 - ratio, 2)
+#     ins_data['ratio'] = ratio
+#     ins_data['rest_ratio'] = rest_ratio
+#     ins_data['data'] = data
+#
+#     count_sample = len(ins_samples)
+#     return render_template('new_detail.html', start_time=start, end_time=end, ins_data=ins_data, all_instruments=all_instruments, selected_ins=selected_ins)
--- server_new/monitor_system/static/select.js
+++ server_new/monitor_system/static/select.js
@@ -1,15 +1,213 @@
 
-console.log("connected!");
-$(".choice").click(function() {
-    $(this).children("input").attr("checked", "checked");
-    $("#span_form").submit();
-
-});
-
-
-
 
 $("#ins_menu").click(function(event){
-    $(".ins_select").toggleClass("not_show");
+    $(".ins").toggleClass("not_show");
     // $(".ins_select").fadeOut(500,function(){})
 });
+
+var datePicker = $("#datePicker");
+var selected_instruments = [];
+
+datePicker.on("submit",function(e){
+       e.preventDefault();
+   });
+
+var name = '';
+var app = {
+    single: $("button#single"),
+    multiple: $("button#multiple"),
+    content: $("section#graph"),
+    ins : $(".ins_select"),
+    multi_ins: $(".multi_ins_select"),
+    span: $("div#span label"),
+    defaultSpan: $("label#default"),
+    sidebar: $("section#sidebar"),
+    confirm: $("button#confirm")
+};
+
+app.init = function(){
+    app.ins.on("click",function(){
+    //    从路径中取page这个data
+    name = $(this).data('name');
+    console.log(name);
+    app.loadPage(name);
+    console.log("clicked instrument");
+    app.span.removeClass('active');
+    app.defaultSpan.addClass('active');
+    $(".ins_select").removeClass("active_ins");
+    $(this).addClass('active_ins');
+    document.querySelector("input[name='start_time']").value = '';
+    document.querySelector("input[name='end_time']").value = '';
+});
+
+    app.multi_ins.on("click",function(){
+    //    从路径中取page这个data
+    name = $(this).data('name');
+    //store selected instrument names into an array
+    if(selected_instruments.indexOf(name)=== -1){
+        console.log("new");
+        $(this).addClass('active_ins');
+        selected_instruments.push(name);
+        console.log(selected_instruments);
+    }
+    //click a selected instrument will cancel the clicked status
+    else{
+       console.log("exist!");
+        $(this).removeClass('active_ins');
+        var index = selected_instruments.indexOf(name);
+        console.log("delete: "+index + selected_instruments[index]);
+        selected_instruments.splice(index,1);
+    }
+    });
+
+
+      app.confirm.on("click",function(){
+          console.log("confirm");
+          app.loadMultiGraph(selected_instruments);
+        });
+
+
+     app.span.on("click",function(){
+
+         if(name){
+             console.log("clicked span!");
+             console.log(typeof(name));
+             console.log(name);
+            var option = $(this).data('span');
+            console.log(option);
+            app.loadSpanPage(name,option);
+         }
+        });
+
+    datePicker.on("submit",function(){
+         if(name){
+             console.log(name);
+             app.span.removeClass('active');
+             var start_time = document.querySelector("input[name='start_time']").value;
+             var end_time = document.querySelector("input[name='end_time']").value;
+             app.loadDatePage(name,start_time,end_time)
+   }
+    });
+
+    app.single.on("click",function(){
+        console.log("single")
+    });
+
+    app.multiple.on("click",function(){
+        app.loadMultipleSidebar()
+    });
+
+    app.single.on("click",function(){
+        app.loadSingleSidebar()
+    });
+
+}();
+
+app.putContent = function(content) {
+    this.content.html(content);
+};
+
+app.putSidebar = function(sidebar) {
+    this.sidebar.html(sidebar)
+}
+
+app.loadPage = function(name){
+      //发送data page，获取新render的html
+      $.ajax({
+        type:'GET',
+          url:'/detail',
+          // dataType:'json',
+        cache:false,
+        data: {name: name},
+        success:function(data){
+            // console.log(data);
+            app.putContent(data);
+            },
+         error: function(){
+            app.putContent("Invalid Input!")}
+        });
+};
+
+app.loadSpanPage = function(name,span){
+      $.ajax({
+        type:'GET',
+          url:'/detail',
+          // dataType:'json',
+        cache:false,
+        data: {name: name, span: span},
+        success:function(data){
+            console.log(name);
+            console.log("hi!!!in the span loadpage");
+            app.putContent(data);
+            },
+         error: function(){
+            app.putContent("Invalid Input!")}
+        });
+};
+
+app.loadDatePage = function(name,start_time,end_time){
+     $.ajax({
+        type:'GET',
+          url:'/detail',
+          // dataType:'json',
+        cache:false,
+        data: {name: name, start_time: start_time, end_time: end_time},
+        success:function(data){
+            console.log(name);
+            console.log("hi!!!in the dates loadpage");
+            app.putContent(data);
+            },
+         error: function(){
+            app.putContent("Invalid Input!")}
+        });
+};
+
+app.loadMultipleSidebar = function(){
+    $.ajax({
+        url:'/detail',
+        type:'GET',
+        cache:false,
+        data:{sidebar: "multiSidebar"},
+        success:function(data){
+            app.putSidebar(data)
+
+        },
+        error: function(){
+            app.putContent("Invalid Input!")}
+    })
+
+};
+
+app.loadSingleSidebar = function(){
+    $.ajax({
+        url:'/detail',
+        type:'GET',
+        cache:false,
+        data:{sidebar: "singleSidebar"},
+        success:function(data){
+            app.putSidebar(data)
+        },
+        error: function(){
+            app.putContent("Invalid Input!")}
+    })
+
+};
+
+app.loadMultiGraph = function(selected_instruments) {
+    var instruments = JSON.stringify(selected_instruments);
+     $.ajax({
+        type:'GET',
+          url:'/detail',
+          // dataType:'json',
+        cache:false,
+         // traditional:true,
+        data: {multi: "multi",selected_instruments:instruments},
+        success:function(data){
+            // console.log(name);
+            console.log("hi!!!in the multi ins loadpage");
+            app.putContent(data);
+            },
+         error: function(){
+            app.putContent("Invalid Input!")}
+        });
+}
